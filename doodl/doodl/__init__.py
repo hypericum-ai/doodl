01dfc2d10bb422555a170284222bb8d976c0f892
Ransome Madziyauswa <rmadziyauswa@gmail.com>
2025-10-22 15:23:33 +0200

    meeting

--- doodl/doodl/__init__.py
+++ doodl/doodl/__init__.py
@@ -1,5 +1,5 @@
 from doodl.doodl import\
     linechart, piechart, skey, barchart, tree, vennchart, gantt, treemap,\
     heatmap, dotplot, scatterplot, boxplot, force, chord, disjoint,\
-    bollinger, dendrogram, contour, areachart, bubblechart, voronoi
+    bollinger, dendrogram, contour, areachart, bubblechart, voronoi, stacked_areachart, stacked_barchart
 from doodl.data import interpret_data
