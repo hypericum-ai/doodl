b2202cae4de85937c9d2497a7c1594083c0da376
Scott Deerwester <scott.deerwester@gmail.com>
2025-11-25 14:29:42 -0500

    Added custom charts from Python Fixed bug in -P argument A bit of refactoring

--- doodl/doodl/__init__.py
+++ doodl/doodl/__init__.py
@@ -2,6 +2,7 @@ from doodl.doodl import \
     areachart, barchart, bollinger, boxplot, bubblechart, chord, \
     contour, dendrogram, disjoint, dotplot, force, gantt, heatmap, \
     linechart, piechart, scatterplot, skey, stacked_areachart, \
-    stacked_barchart, tree, treemap, vennchart, voronoi
+    stacked_barchart, tree, treemap, vennchart, voronoi, \
+    register_chart
 
 from doodl.data import interpret_data
